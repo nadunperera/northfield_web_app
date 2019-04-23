8001a12ab8422332a212f3df118d6a743517985a
Nadun Perera <nudie.perera@gmail.com>
2019-04-23 14:20:05 +1000

    added dynamically changing login logout links based on the active user session.

--- src/northfield_web_app/urls.py
+++ src/northfield_web_app/urls.py
@@ -23,9 +23,9 @@ from user import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_page, name='home_page'),
-    path('register/', user_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='../templates/user_login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='../templates/user_logout.html'), name='logout'),
+    path('register/', user_views.register, name='user_register'),
+    path('login/', auth_views.LoginView.as_view(template_name='../templates/user_login.html'), name='user_login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='../templates/user_logout.html'), name='user_logout'),
 
     path('asset/', include('asset.urls')),
 ]
--- src/user/views.py
+++ src/user/views.py
@@ -9,9 +9,8 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created! You are now able to log in.')
-            return redirect('login')
+            return redirect('user_login')
     else:
         form = UserRegisterForm()
     template_name = 'user_register.html'
