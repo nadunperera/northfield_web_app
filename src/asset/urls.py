1f999738537004a2ee1fae3662bba0fde99afea0
Nadun Perera <nudie.perera@gmail.com>
2019-04-23 22:38:27 +1000

    asset add/edit/view working.

--- src/asset/urls.py
+++ src/asset/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import AssetListView
-from . import views
+from .views import AssetListView, AssetDetailView, AssetCreateView, AssetUpdateView
 
 urlpatterns = [
     path('', AssetListView.as_view(), name='asset_list'),
-    path('<int:asset_id>/', views.asset_detail),
+    path('asset/<int:pk>/', AssetDetailView.as_view(), name='asset_detail'),
+    path('asset/new/', AssetCreateView.as_view(), name='asset_create'),
+    path('asset/<int:pk>/update/', AssetUpdateView.as_view(), name='asset_update'),
 ]
--- src/asset/views.py
+++ src/asset/views.py
@@ -1,28 +1,25 @@
-from django.shortcuts import render, get_object_or_404
-from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Asset
 
 
 # Create your views here.
-class AssetListView(ListView):
+class AssetListView(LoginRequiredMixin, ListView):
     model = Asset
     context_object_name = 'assets'
     ordering = ['-created']
 
-# @login_required
-# def asset_list(request):
-#     template_name = 'asset_list.html'
-#     context = {'assets': Asset.objects.all(),
-#                'page_title': 'All Assets',
-#                'asset_link': 'active'}
-#     return render(request, template_name, context)
-#
-@login_required
-def asset_detail(request, asset_id):
-    asset = get_object_or_404(Asset, id=asset_id)
-    template_name = 'asset/asset_detail.html'
-    context = {'asset': asset,
-               'page_title': 'Assets | ' + asset.name,
-               'asset_link': 'active'}
-    return render(request, template_name, context)
+
+class AssetDetailView(LoginRequiredMixin, DetailView):
+    model = Asset
+    context_object_name = 'asset'
+
+
+class AssetCreateView(LoginRequiredMixin, CreateView):
+    model = Asset
+    fields = ['name', 'address', 'suburb', 'postcode', 'state']
+
+
+class AssetUpdateView(LoginRequiredMixin, UpdateView):
+    model = Asset
+    fields = ['name', 'address', 'suburb', 'postcode', 'state']
